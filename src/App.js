3f73cf37dd5a16275403b7cfd7174b870a49d4a5
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-04 21:16:19 +0600

    added the React-Router library and added links to other pages of the site via Link and made them work via route route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Link, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div className="App">
-    <h1>Hello world</h1>
-    
-  </div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+
+      <Link to="/">Home</Link>
+      <Link to="/phones">Phones</Link>
+      <Link to="mypage">My page</Link>
+      <Link to="MyWebsite"> My webSite </Link>
+      <Link to="MySun">My Sun</Link>
+
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/phones" element={<h2>Phones Page</h2>} />
+        <Route path="/mypage" element={<h2>My page</h2>} />
+        <Route path="/MyWebsite" element={<h2>My webSite</h2>} /> 
+        <Route path="/MySun" element={<h1>My Sun</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
