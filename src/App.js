0f628cd16bc59ac78afb6a563c3864d7e0aa533f
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-05 15:26:30 +0600

    created a Layout inside it created routes links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-
-      <Link to="/">Home</Link>
-      <Link to="/phones">Phones</Link>
-      <Link to="mypage">My page</Link>
-
-      <Routes>
-        <Route path="/" element={<h2>Home Page</h2>} />
-        <Route path="/phones" element={<h2>Phones Page</h2>} />
-        <Route path="/mypage" element={<h2>My page</h2>} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/delivery" element={<h1>Delivery</h1>} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
