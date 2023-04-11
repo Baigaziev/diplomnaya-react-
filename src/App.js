01c678cca8935a9aa19da11a283765bcdcda118a
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-11 16:22:57 +0600

    В этом файле определяется основной компонент приложения - App. В функциональном компоненте App определены маршруты приложения с помощью компонента Routes из библиотеки react-router-dom. Каждый маршрут определяется с помощью компонента Route и атрибутов path и element, которые определяют, какой компонент должен быть отображен при соответствующем URL-адресе. Также используется компонент Layout, который отображает общую структуру страницы.
    В итоге в этом коде было определено маршрутизация для различных страниц приложения и был использован компонент Layout для общей структуры страницы. Компоненты Home, About, Contact, Delivery, Category и NotFound были определены ранее и использованы в этом файле для соответствующих маршрутов

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
- 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Delivery from "./pages/Delivery";
+import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
+
+
+
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
-          <Route path="/delivery" element={<h1>Delivery</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/categories/:slug" element={<Category />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </div>
