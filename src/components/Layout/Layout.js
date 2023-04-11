95aaa74b50f32fb213711199b38537a1979bfb7c
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-11 16:40:17 +0600

    Layout.js file created Imported components Nav, Logo, CategoryList. The Layout component has been created. The Layout component contains header, aside, main and footer blocks. The Logo and Nav components are rendered in the header block. The CategoryList component is rendered in the aside block. In the main block, the child elements passed through props are rendered. Footer added.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Layout.css";
+import Logo from "../Logo/Logo";
+import CategoryList from "../CategoryList/CategoryList";
+
+export default function Layout(props) {
+  return (
+    <div className="Layout">
+      <header>
+        <Logo />
+        <Nav />
+      </header>
+
+      <aside>
+       <CategoryList />
+      </aside>
+
+      <main>{props.children}</main>
+
+      <footer>FOOTER</footer>
+    </div>
+  );
+}
