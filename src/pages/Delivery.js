7f50562d282d1ea62f40da99f053402a7bc919c8
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-11 16:27:05 +0600

    This code is a Delivery component that renders a title and a paragraph of text. It was created to display a shipping page on a website. Thus, the Delivery component was created and added to the application.

---
+++ src/pages/Delivery.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Delivery() {
+  return (
+    <div className='Delivery'>
+      <h1>Delivery</h1>
+      <p>This is my page</p>
+      </div>
+  )
+}
