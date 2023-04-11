79819f9417aca65831da4a36d4964f6f2959c57c
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-11 16:37:30 +0600

    A new "Logo" component has been added, which imports the logo from the "logo.png" file and displays it on the page.   "Add NavLink to Logo component"
    Added a NavLink component to the Logo component to make the logo clickable and link it to the main page of the site.
    
      "Style Logo component"
    
    Added styles to the Logo component to make it more attractive and consistent with the rest of the site's design.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import logo from "../../asests/logo.png"
+import { NavLink } from "react-router-dom";
+import "./Logo.css";
+export default function Logo() {
+  return (
+    <div className="Logo">
+      <NavLink>
+        <img src={logo} alt="logo" />
+      </NavLink>
+    </div>
+  );
+}
