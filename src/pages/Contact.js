92dd2c0b1e533e2cc2b4ca2ed7a21f9d29fb5f65
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-11 16:28:34 +0600

    is the Contact component, which is responsible for displaying the contact page. This component defines a div container with the class "Contact", which contains the title of the page and some text. This component is used in the application's routing to show the contact page based on the URL path. Let's make a commit with the description "Added a Contact component for the contact page".

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className='Contact'>
+      <h1>Contact</h1>
+      <p>This is my page</p>
+      </div>
+  )
+}
