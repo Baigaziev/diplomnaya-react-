88431cb5e79800dfc3dac8d5e34102437c7689f9
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-12 21:03:58 +0600

    new component exported product from firebas

---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
+import "./ProductList.css";
+
+import React, { useContext } from "react";
+
+export default function ProductList() {
+  const { products } = useContext(AppContext);
+
+  const output =products.map(product => (
+    <div>
+      <img className="keyboards" src={product.picture} alt={product.name} />
+      <NavLink to={'/product/' + product.slug}>
+        {product.name}
+      </NavLink>
+      <span>{product.price} dollar</span>
+    </div>
+  ))
+  return <div className="ProductList">
+    {output}
+  </div>;
+}
