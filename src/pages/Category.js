b8192ee3c17351e3f89604482a1679902fcc4b5a
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-12 15:20:13 +0600

    removed the constant that was passing the categories instead just briefly passed the AppContext

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,18 +1,12 @@
 import { useMatch } from "react-router-dom";
 import NotFound from "./NotFound";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Category() {
   const {params}=useMatch("/categories/:slug/");
 
-  const categories = [
-    {id:1,name:" Keyboards", slug:"keyboards"},
-    {id:2,name:"Mice", slug:"mice"},
-    {id:3,name:"Headphones",slug:"headphones"},
-    {id:4,name:"Joysticks ", slug:"joysticks"},
-    {id:5,name:"Armchairs",slug:"armchairs"},
-    {id:6,name:" Tablets",slug:"tablets"},
-    {id:7,name:"Computers ",slug:"computers"},
-  ];
+  const{ categories }= useContext(AppContext)
 
   const category = categories.find( 
     (category) => category.slug===params.slug
