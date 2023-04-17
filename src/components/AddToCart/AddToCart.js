f1d1266f9664746bf725b960241270b09612e37b
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-17 14:34:41 +0600

    created a function inside the function, made it so that when the button is clicked, the product will increase

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import "./AddToCart.css"
+import React, { useContext } from "react";
+import "./AddToCart.css";
+import { AppContext } from "../../App";
 
-export default function AddToCart({product}) {
+export default function AddToCart({ product }) {
+  const { cart, setCart } = useContext(AppContext);
+
+  function onAddToClick() {
+    const qty = cart[product.id] ? cart[product.id] + 1 : 1;
+    setCart({
+      ...cart, //!Положить текущее содержимое в корзинке
+      [product.id]: qty, //! Добавить текущий товар
+    });
+  }
   return (
-    <div className='AddToCart'>
-      <button>Add to cart</button>
-      </div>
-  )
+    <div className="AddToCart">
+      {cart[product.id] ? cart[product.id] : 0}
+      <button onClick={onAddToClick}>Add to cart</button>
+    </div>
+  );
 }
