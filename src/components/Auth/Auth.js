e6dbe4a630009bc82420c2a56ab0944e1f972233
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-19 18:01:06 +0600

    In this code, I've created an Auth functional bean that uses useContext to get the user from the global state provided through the context from the App bean. Then, if user is not defined, it returns the "SignIn" button with the button-one class and the signIn click handler, otherwise it displays the user's displayName and the "SignOut" button with the button-two class and the signOff click handler.

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { signIn, signOff } from "../../firebase";
+import { AppContext } from "../../App";
+
+export default function Auth() {
+  const { user } = useContext(AppContext);
+
+  return (
+    <div className="Auth">
+      {!user ? <button className="button-one" onClick={signIn}>SignIn</button> : null}
+      {user ? <span>{user.displayName}</span> : null}
+      {user ? <button className="button-two" onClick={signOff}>SignOut</button> : null}
+    </div>
+  );
+}
