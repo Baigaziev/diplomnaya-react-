f69e8be7bb8c370690c2fa9cebd9a4ee7b55f930
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-19 18:01:58 +0600

    fixed indentation

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -11,7 +11,9 @@ export default function CartLink() {
   const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
   return (
     <div className="CartLink">
-      <NavLink to="/cart">Cart ({total})</NavLink>
+      <NavLink to="/cart">
+        Cart({total})
+        </NavLink>
     </div>
   );
 }
