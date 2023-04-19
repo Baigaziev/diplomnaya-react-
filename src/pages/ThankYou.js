acd7c2107bfa78dd3c1f9cca16282cfd09603495
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-19 15:03:46 +0600

    here I made it so that after the purchase we would thank the client for the purchase

---
+++ src/pages/ThankYou.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ThankYou() {
+  return (
+    <div className="ThankYou">
+      <h1>Thenk Yoy!</h1>
+      <p>
+        Your order has been received. Our specialists will contact with you
+        shortly to confirm it.
+      </p>
+    </div>
+  );
+}
