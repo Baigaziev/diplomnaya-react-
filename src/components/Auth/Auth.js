bf24bb47cd24d6db24d776afe25976ef7ff205c3
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-24 14:59:15 +0600

    changed sigIn and sigUout to logIn and logOut

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { signIn, signOff } from "../../firebase";
+import { logIn, logOut } from "../../firebase";
 import { AppContext } from "../../App";
 
 export default function Auth() {
@@ -7,9 +7,9 @@ export default function Auth() {
 
   return (
     <div className="Auth">
-      {!user ? <button className="button-one" onClick={signIn}>Sign In</button> : null}
+      {!user ? <button className="button-one" onClick={logIn}>SignIn</button> : null}
       {user ? <span>{user.displayName}</span> : null}
-      {user ? <button className="button-two" onClick={signOff}>Sign Ut</button> : null}
+      {user ? <button className="button-two" onClick={logOut}>SignUp</button> : null}
     </div>
   );
 }
