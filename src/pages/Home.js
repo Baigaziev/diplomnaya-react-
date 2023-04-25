51527605624f1fdd4c252cc1fcf4ec002a387624
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-04-25 20:58:56 +0600

    added an input so that it would be possible to search for products within the site

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
-      <p>This is my page</p>
+      <form>
+        <label htmlFor="search">Search:</label>
+        <input type="text" id="search" name="search" />
+        <button type="submit">Search</button>
+      </form>
+      <div className="welcome-message">
+        <h2>Welcome to our gadget store for gamers!</h2>
+        <p>Here you can find the latest and greatest gaming gadgets, all in one place.</p>
+      </div>
     </div>
   );
 }
+
