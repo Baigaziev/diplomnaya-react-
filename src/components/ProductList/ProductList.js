669891cc5580da2a0b044b8b35d689dfa925df93
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-05-11 13:12:30 +0600

    imported the deleteproduct component into the categoricallist component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -5,6 +5,7 @@ import "./ProductList.css";
 import React, { useContext } from "react";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
+import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
@@ -15,14 +16,18 @@ export default function ProductList({ category }) {
       <div key={product.id} className="ProductList__item">
         <img src={product.picture} alt={product.name} />
         <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
-        <span>         {product.price} $</span>
+        <span> {product.price} $</span>
         <AddToCart product={product} />
+        <DeleteProduct product={product} />
       </div>
     ));
 
-  return <div className="ProductList">
-    {output}
+  return (
+    <div className="ProductList">
+      
+      {output}
 
-    <AddProduct />
-    </div>;
+      <AddProduct category={category} />
+    </div>
+  );
 }
