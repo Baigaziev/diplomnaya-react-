004578c229e004dcc542803d8503cc5c66b9b21c
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-05-18 14:12:56 +0600

    imported nav togle into Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../Nav/Nav";
 import "./Layout.css";
 import Logo from "../Logo/Logo";
@@ -7,13 +7,22 @@ import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
 import { Slider } from "../Slide/Slide";
 import Footer from "../Footer/Footer";
+import NavToggle from "../NavToggle/NavToggle";
+import Drawer from "../Drawer/Drawer";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  
+    function toggleDrawer() {
+      setDrawerOpen(!drawerOpen);
+    }
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
         <CartLink />
         <Auth />
       </header>
