a77ee251831d1dce61005584dfca4a7de08d9c46
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-05-25 10:10:57 +0600

    removed search engine function in Home.js and Home.css removed styles for this search engine

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,10 @@
-import React, { useState } from "react";
+
 import "./Home.css";
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [previousSearch, setPreviousSearch] = useState("");
-  const [searchError, setSearchError] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const matchingProduct = product.find(
-      (product) => product.id === parseInt(searchTerm)
-    );
-    if (matchingProduct) {
-      setSearchError(false);
-      window.location.href = `/product/${matchingProduct.id}`;
-    } else {
-      setSearchError(true);
-      console.log(`Product with ID "${searchTerm}" not found`);
-    }
-    setPreviousSearch(searchTerm);
-    setSearchTerm("");
-  };
-
   return (
+    
     <div className="Home">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="search">Search:</label>
-        <input
-          placeholder="search by product"
-          type="text"
-          id="search"
-          name="search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-      {searchError && (
-        <p className="search-error">Product not found. Please try again.</p>
-      )}
-      {previousSearch && (
-        <p className="previous-search">
-          Your previous search was for product with ID "{previousSearch}"
-        </p>
-      )}
       <div className="welcome-message">
         <h2 className="fade-in">Welcome to our gadget store for gamers!</h2>
         <p className="fade-in">
@@ -146,54 +107,3 @@ export default function Home() {
     </div>
   );
 }
-
-const product = [
-  {
-    id: 1,
-    name: "Sony DualSense Edge",
-    description:
-      "The Sony DualSense Edge is a high-performance controller designed specifically for the PlayStation 5.",
-    image: "https://via.placeholder.com/150",
-    category: "Joysticks",
-  },
-  {
-    id: 2,
-    name: "Anda Seat! AD12XL",
-    description:
-      "The Anda Seat! AD12XL is a premium gaming chair that provides maximum comfort during long gaming sessions.",
-    image: "https://via.placeholder.com/150",
-    category: "chairs",
-  },
-  {
-    id: 3,
-    name: "HyperX Cloud Alpha S",
-    description:
-      "The HyperX Cloud Alpha S is a high-quality gaming headset that features 7.1 surround sound and a detachable noise-cancelling microphone.",
-    image: "https://via.placeholder.com/150",
-    category: "headsets",
-  },
-  {
-    id: 4,
-    name: "Logitech G915 TKL",
-    description:
-      "The Logitech G915 TKL is a wireless gaming keyboard that offers a low-profile design, programmable keys, and customizable RGB lighting.",
-    image: "https://via.placeholder.com/150",
-    category: "keyboards",
-  },
-  {
-    id: 5,
-    name: "Alienware m15 R6",
-    description:
-      "The Alienware m15 R6 is a powerful gaming laptop that features an Intel Core i7 processor, NVIDIA GeForce RTX graphics, and a 15.6-inch display with a 240Hz refresh rate.",
-    image: "https://via.placeholder.com/150",
-    category: "laptops",
-  },
-  {
-    id: 6,
-    name: "ASUS ROG Swift PG279QM",
-    description:
-      "The ASUS ROG Swift PG279QM is a high-end gaming monitor that features a 27-inch QHD display with a 240Hz refresh rate and NVIDIA G-SYNC technology.",
-    image: "https://via.placeholder.com/150",
-    category: "monitors",
-  },
-];
